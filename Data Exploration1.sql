69bba093d9819fdd7aa25ad19c40dce6b4c939d5
Karthi5672001 <118644207+Karthi5672001@users.noreply.github.com>
2022-11-20 02:58:23 +0000

    Add files via upload

--- Data Exploration1.sql
+++ Data Exploration1.sql
@@ -1,28 +1,28 @@
---Select * 
---From [Portfolio Project1]..coviddeaths$
---order by 3,4
+Select * 
+From [Portfolio Project1]..coviddeaths$
+order by 3,4
 
---Select * 
---From [Portfolio Project1]..Covidvaccs$
---order by 3,4
+Select * 
+From [Portfolio Project1]..Covidvaccs$
+order by 3,4
 
----- Selecting the necessary Data
+-- Selecting the necessary Data
 
---Select Location,date, total_cases, new_cases, total_deaths, population
---From [Portfolio Project1]..coviddeaths$
---order by 1,2
+Select Location,date, total_cases, new_cases, total_deaths, population
+From [Portfolio Project1]..coviddeaths$
+order by 1,2
 
-----Looking at total_cases vs total_deaths in UK as percentage mortalility
---Select Location,date, total_cases, total_deaths, (total_deaths/total_cases)*100 as Mortalitypercent
---From [Portfolio Project1]..coviddeaths$
---Where Location like '%kingdom%'
---order by 1,2
+--Looking at total_cases vs total_deaths in UK as percentage mortalility
+Select Location,date, total_cases, total_deaths, (total_deaths/total_cases)*100 as Mortalitypercent
+From [Portfolio Project1]..coviddeaths$
+Where Location like '%kingdom%'
+order by 1,2
 
----- total_cases vs population
---Select Location,date, total_cases, population, (total_cases/population)*100 as PopulationInfectionpercent
---From [Portfolio Project1]..coviddeaths$
---Where Location like '%kingdom%'
---order by 1,2
+-- total_cases vs population
+Select Location,date, total_cases, population, (total_cases/population)*100 as PopulationInfectionpercent
+From [Portfolio Project1]..coviddeaths$
+Where Location like '%kingdom%'
+order by 1,2
 
 -- Looking at Countries with highest infection rate
 Select Location, population, MAX(total_cases) as HighestInfectionCount,  MAX((total_cases/population))*100 as Casepercent
@@ -123,4 +123,34 @@ join [Portfolio Project1]..coviddeaths$ dea
 on dea.location = vac.location
 and dea.date = vac.date
 Where dea.continent is not null
+--order by 2,3
+
+Create View Sumofpeopledeadpercent as
+Select dea.continent, dea.location, dea.date, population, dea.new_deaths
+, SUM(CONVERT(int, dea.new_deaths)) OVER (Partition by dea.location Order by dea.location, dea.Date ) as Sumofpeopledead
+From [Portfolio Project1]..Covidvaccs$ vac
+join [Portfolio Project1]..coviddeaths$ dea
+on dea.location = vac.location
+and dea.date = vac.date
+Where dea.continent is not null
+--order by 2,3
+
+Create View Sumofpeoplevaccinatedpercentuk as
+Select dea.continent, dea.location, dea.date, population, vac.new_vaccinations
+, SUM(CONVERT(int, vac.new_vaccinations)) OVER (Partition by dea.location Order by dea.location, dea.Date ) as Sumofpeoplevaccinated
+From [Portfolio Project1]..Covidvaccs$ vac
+join [Portfolio Project1]..coviddeaths$ dea
+on dea.location = vac.location
+and dea.date = vac.date
+Where dea.Location like '%kingdom%'
+--order by 2,3
+
+Create View Sumofpeopledeadpercentuk as
+Select dea.continent, dea.location, dea.date, population, dea.new_deaths
+, SUM(CONVERT(int, dea.new_deaths)) OVER (Partition by dea.location Order by dea.location, dea.Date ) as Sumofpeopledead
+From [Portfolio Project1]..Covidvaccs$ vac
+join [Portfolio Project1]..coviddeaths$ dea
+on dea.location = vac.location
+and dea.date = vac.date
+Where dea.Location like '%kingdom%'
 --order by 2,3
